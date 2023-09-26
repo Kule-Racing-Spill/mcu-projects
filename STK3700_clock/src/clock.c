a1d59922e6137a12a34a5198408f472c777ce53a
magnetenstad <tenstad.magne@gmail.com>
2023-09-26 02:29:08 +0200

    Update clock.c

--- STK3700_clock/src/clock.c
+++ STK3700_clock/src/clock.c
@@ -88,130 +88,6 @@ void gpioSetup(void)
 }
 
 /***************************************************************************//**
- * @brief RTC Interrupt Handler.
- *        Updates minutes and hours.
- ******************************************************************************/
-void RTC_IRQHandler(void)
-{
-  /* Clear interrupt source */
-  RTC_IntClear(RTC_IFC_COMP0);
-
-  /* Increase time by one minute */
-  minutes++;
-  if (minutes > 59) {
-    minutes = 0;
-    hours++;
-    if (hours > 23) {
-      hours = 0;
-    }
-  }
-}
-
-/***************************************************************************//**
- * @brief Enables LFACLK and selects LFXO as clock source for RTC
- *        Sets up the RTC to generate an interrupt every minute.
- ******************************************************************************/
-void rtcSetup(void)
-{
-  RTC_Init_TypeDef rtcInit = RTC_INIT_DEFAULT;
-
-  /* Enable LE domain registers */
-  CMU_ClockEnable(cmuClock_CORELE, true);
-
-  /* Enable LFXO as LFACLK in CMU. This will also start LFXO */
-  CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
-
-  /* Set a clock divisor of 32 to reduce power conumption. */
-  CMU_ClockDivSet(cmuClock_RTC, cmuClkDiv_32);
-
-  /* Enable RTC clock */
-  CMU_ClockEnable(cmuClock_RTC, true);
-
-  /* Initialize RTC */
-  rtcInit.enable   = false;  /* Do not start RTC after initialization is complete. */
-  rtcInit.debugRun = false;  /* Halt RTC when debugging. */
-  rtcInit.comp0Top = true;   /* Wrap around on COMP0 match. */
-  RTC_Init(&rtcInit);
-
-  /* Interrupt every minute */
-  RTC_CompareSet(0, ((RTC_FREQ / 32) * 60) - 1);
-
-  /* Enable interrupt */
-  NVIC_EnableIRQ(RTC_IRQn);
-  RTC_IntEnable(RTC_IEN_COMP0);
-
-  /* Start Counter */
-  RTC_Enable(true);
-}
-
-/***************************************************************************//**
- * @brief Check input voltage and enable vboost if it drops too low.
- ******************************************************************************/
-void checkVoltage(void)
-{
-  bool vboost;
-
-  /* Initialize voltage comparator */
-  VDDCHECK_Init();
-
-  /* Check if voltage is below 3V, if so use voltage boost */
-  if (VDDCHECK_LowVoltage(2.9)) {
-    vboost = true;
-  } else {
-    vboost = false;
-  }
-
-  /* Disable Voltage Comparator */
-  VDDCHECK_Disable();
-
-  if (vboost != oldBoost) {
-    /* Reinitialize with new vboost setting */
-    SegmentLCD_Init(vboost);
-    /* Use Antenna symbol to signify enabling of vboost */
-    SegmentLCD_Symbol(LCD_SYMBOL_ANT, vboost);
-    oldBoost = vboost;
-  }
-}
-
-/***************************************************************************//**
- * @brief Update clock and wait in EM2 for RTC tick.
- ******************************************************************************/
-void clockLoop(void)
-{
-  LCD_FrameCountInit_TypeDef frameInit;
-  LCD_AnimInit_TypeDef animInit;
-
-  /* Write Gecko and display, and light up the colon between hours and minutes. */
-  SegmentLCD_Symbol(LCD_SYMBOL_COL10, 1);
-  SegmentLCD_Write("Giant");
-
-  /* Setup frame counter */
-  frameInit.enable   = true;           /* Enable framecounter */
-  frameInit.top      = 15;             /* Generate event every 15 frames. */
-  frameInit.prescale = lcdFCPrescDiv1; /* No prescaling */
-
-  LCD_FrameCountInit(&frameInit);
-
-  /* Animate half ring - by special board design it is possible to achieve */
-  /* "slide in/slide out" effect                                           */
-  animInit.enable    = true;             /* Enable animation after initialization. */
-  animInit.AReg      = 0x00;             /* Initial A register value */
-  animInit.BReg      = 0x0F;             /* Initial B register value */
-  animInit.AShift    = lcdAnimShiftLeft; /* Shift A register left */
-  animInit.BShift    = lcdAnimShiftLeft; /* Shift B register left */
-  animInit.animLogic = lcdAnimLogicOr;   /* Enable segment if A or B */
-  animInit.startSeg  = 8;                /* Initial animation segment */
-
-  LCD_AnimInit(&animInit);
-
-  while (1) {
-    checkVoltage();
-    SegmentLCD_Number(hours * 100 + minutes);
-    EMU_EnterEM2(true);
-  }
-}
-
-/***************************************************************************//**
  * @brief  Main function
  ******************************************************************************/
 int main(void)
@@ -219,23 +95,19 @@ int main(void)
   /* Chip errata */
   CHIP_Init();
 
-  /* If first word of user data page is non-zero, enable Energy Profiler trace */
-  BSP_TraceProfilerSetup();
-
   /* Ensure core frequency has been updated */
   SystemCoreClockUpdate();
 
   /* Init LCD with no voltage boost */
   SegmentLCD_Init(oldBoost);
 
-  /* Setup RTC to generate an interrupt every minute */
-  rtcSetup();
-
   /* Setup GPIO with interrupts to serve the pushbuttons */
   gpioSetup();
 
   /* Main function loop */
-  clockLoop();
+  while (1) {
+	  SegmentLCD_Number(hours * 100 + minutes);
+  }
 
   return 0;
 }
