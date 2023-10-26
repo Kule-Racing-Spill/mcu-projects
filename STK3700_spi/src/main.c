7536cd80197dae82c4c29e279961469ad3b0f894
magnetenstad <tenstad.magne@gmail.com>
2023-10-26 12:54:24 +0200

    SPI: Move deps to src/lib

--- STK3700_spi/src/main.c
+++ STK3700_spi/src/main.c
@@ -1,5 +1,5 @@
-#include "spidrv.h"
-#include "segmentlcd.h"
+#include "lib/spidrv.h"
+#include "lib/segmentlcd.h"
 
 void TransferComplete( SPIDRV_Handle_t handle,
                        Ecode_t transferStatus,
@@ -15,6 +15,13 @@ void TransferComplete( SPIDRV_Handle_t handle,
 SPIDRV_HandleData_t handleData;
 SPIDRV_Handle_t handle = &handleData;
 
+#define NUM_SPRITE_IDS 4
+#define SPRITE_BYTES (32*16)
+#define SPI_CMD_SEND_SPRITE 0
+#define SPI_CMD_DRAW_SPRITES 1
+#define SPI_CMD_TEST_2 2
+#define SPI_CMD_TEST_3 3
+
 struct sprite_draw_info {
 	uint8_t sprite_id;
 	uint16_t x;
@@ -24,47 +31,60 @@ struct sprite_draw_info {
 
 // a pixel is 4 bits
 // a sprite is 32x32 pixels
-uint8_t *sprite_0[32*16];
-uint8_t *sprite_1[32*16];
-uint8_t *sprite_2[32*16];
-uint8_t *sprite_3[32*16];
+uint8_t sprite_0[SPRITE_BYTES];
+uint8_t sprite_1[SPRITE_BYTES];
+uint8_t sprite_2[SPRITE_BYTES];
+uint8_t sprite_3[SPRITE_BYTES];
 
-uint8_t **sprites[4] = {
+uint8_t *sprites[4] = {
 		sprite_0,
 		sprite_1,
 		sprite_2,
 		sprite_3
 };
 
-#define NUM_SPRITE_IDS 4
-
-#define SPI_CMD_SEND_SPRITE 0
-#define SPI_CMD_DRAW_SPRITES 1
-
 void spi_send_sprite(uint8_t sprite_id) {
-	uint8_t buffer[2 + 32*16];
+	SegmentLCD_Number(SPI_CMD_SEND_SPRITE);
+	int buffer_size = 2 + SPRITE_BYTES;
+	uint8_t buffer[buffer_size];
 	buffer[0] = SPI_CMD_SEND_SPRITE;
 	buffer[1] = sprite_id;
-	buffer[2] = **sprites[sprite_id];
-	SPIDRV_MTransmitB( handle, buffer, 10 );
+	buffer[2] = *sprites[sprite_id];
+	SPIDRV_MTransmitB( handle, buffer, buffer_size );
 }
 
-void spi_draw_sprites(sprite_draw_info *sprites, uint16_t n) {
-	uint8_t buffer[3 + n * sizeof(sprite_draw_info)];
+void spi_draw_sprites(sprite_draw_info sprite_info) {
+	SegmentLCD_Number(SPI_CMD_DRAW_SPRITES);
+	int buffer_size = 7;
+	uint8_t buffer[buffer_size];
 	buffer[0] = SPI_CMD_DRAW_SPRITES;
-	buffer[1] = n & 0xF0;
-	buffer[2] = n & 0x0F;
-	buffer[3] = *(uint8_t*)sprites;
-	SPIDRV_MTransmitB( handle, buffer, 10 );
+	//buffer[1] = (uint8_t)sprite_info;
+	SPIDRV_MTransmitB( handle, buffer, buffer_size );
+}
+
+void spi_send_test(int i) {
+	SegmentLCD_Number(i);
+	int buffer_size = 1;
+	uint8_t buffer[1];
+	buffer[0] = i;
+	SPIDRV_MTransmitB( handle, buffer, buffer_size );
+}
+
+void busy_sleep(int i) {
+	int c = 0;
+	while (i * 1000 > c) {
+		c++;
+	}
 }
 
 int main( void )
 {
 	SegmentLCD_Init(false);
-	SegmentLCD_Number(0);
+	SegmentLCD_Number(-1);
 
 	//uint8_t buffer[10];
-	SPIDRV_Init_t initData = SPIDRV_MASTER_USART2;
+	SPIDRV_Init_t initData = SPIDRV_MASTER_USART1;
+	initData.bitRate = 1;
 
 	// Initialize a SPI driver instance
 	SPIDRV_Init( handle, &initData );
@@ -76,15 +96,20 @@ int main( void )
 	// Transmit data using a callback to catch transfer completion.
 	//SPIDRV_MTransmit( handle, buffer, 10, TransferComplete );
 
-	for (int i = 0; i < NUM_SPRITE_IDS; i++) {
-		spi_send_sprite(i);
-	}
+	// for (int i = 0; i < NUM_SPRITE_IDS; i++) {
+	// 	spi_send_sprite(i);
+	// }
 
-
-	int n_sprites = 2;
-	sprite_draw_info sprites[n_sprites];
+	sprite_draw_info sprite_info;
 
 	while (1) {
-		spi_draw_sprites(sprites, n_sprites);
+		spi_send_sprite(0);
+		busy_sleep(1000);
+		spi_draw_sprites(sprite_info);
+		busy_sleep(1000);
+		for (int i = 2; i < 10; i++) {
+			spi_send_test(i);
+			busy_sleep(1000);
+		}
 	}
 }
