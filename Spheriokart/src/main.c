9789f5531310ab89a44f11031fd1fcea823ef854
Odd Magne Gynnild <Gynnild14@gmail.com>
2023-11-09 14:17:24 +0100

    added button polling stuff

--- Spheriokart/src/main.c
+++ Spheriokart/src/main.c
@@ -4,12 +4,18 @@
 #include "kart.h"
 #include "printing.h"
 #include "spi.h"
+#include "em_device.h"
+#include "button.h"
 
 int main(){
 	TrackballValues v;
 	SWO_SetupForPrint(); /* For adding printing to console in simplicity studio debugger */
 
-	spi_init();
+	spi_init(); /* SPI init */
+
+	/* Button stuff */
+	Button_Init(BUTTON1|BUTTON2);
+	uint32_t b;
 
 	for(;;){ // Keep trying to connect
 
@@ -17,6 +23,13 @@ int main(){
 		InitTrackball(); /* Initialize the trackball */
 		while(USBH_DeviceConnected()){ /* Program loop */
 			GetTrackballValues(&v);
+			b = Button_ReadReleased();
+			if( b&BUTTON1 ) {
+				printf("Button1 pressed!\n");
+			}
+			if( b&BUTTON2 ) {
+				printf("Button2 pressed!\n");
+			}
 			kart(v);
 		}
 
@@ -27,6 +40,5 @@ int main(){
 		SegmentLCD_Write("Removed");
 		USBTIMER_DelayMs(500);
 		SegmentLCD_Write("USBHOST");
-
 	}
 }
